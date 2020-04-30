1da9028e0d80f1cb2db619091da44dcef9712acb
Paolo <p.fiaschi@hotmail.it>
2020-04-30 16:34:28 +0200

    aggiunto menu e scroll menu js

--- dist/app.js
+++ dist/app.js
@@ -93,7 +93,25 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-console.log("ciao");
+$(document).ready(function () {
+  // HEADER------------
+  var menu = $(".menuTendina");
+  menu.mouseenter(function () {
+    $(".menu").show();
+    $(this).find('a').css('color', '#30488F');
+  });
+  menu.mouseleave(function () {
+    $(".menu").hide();
+    $(this).find('a').css('color', '#58402C');
+  });
+  $(window).scroll(function () {
+    if ($(document).scrollTop() > 70) {
+      $("header").addClass("scrollHeader");
+    } else {
+      $("header").removeClass("scrollHeader");
+    }
+  });
+});
 
 /***/ }),
 
